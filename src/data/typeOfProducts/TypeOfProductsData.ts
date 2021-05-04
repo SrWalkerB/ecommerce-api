1013a73b130410608617327d75fb1b70fa43cb5c
srwalkerb <srwalkerbdevs@gmail.com>
2021-05-03 21:01:10 -0300

    fix: adjust typeOfProductsData

--- src/data/typeOfProducts/TypeOfProductsData.ts
+++ src/data/typeOfProducts/TypeOfProductsData.ts
@@ -2,16 +2,16 @@ import dbActions from '../../database/connect'
 
 export default new class TypeOfProducts {
   async listAllTypeOfProducts () {
-    return await dbActions('typeOfProducts')
+    return await dbActions('type_of_products')
   }
 
   async searchTypeID (id: string) {
-    return await dbActions('typeOfProducts')
+    return await dbActions('type_of_products')
       .where('id_typeProduct', id)
   }
 
   async searchTypeName (name: string) {
-    return await dbActions('typeOfProducts')
+    return await dbActions('type_of_products')
       .where('id_typeProduct', name)
   }
 }()
