4e6dbc05e41a146dc7a413a48b48b5b7a67a61da
srwalkerb <srwalkerbdevs@gmail.com>
2021-05-10 18:57:12 -0300

    feat: adding autenticate in routes favorites

--- src/routes/clientRoutes.ts
+++ src/routes/clientRoutes.ts
@@ -6,10 +6,10 @@ const clientRoutes = Router()
 
 clientRoutes.post('/client', ClientControllers.createClient)
 
-clientRoutes.get('/client/favorite', ClientControllers.myListFavoritesProduct)
+clientRoutes.get('/client/favorite', autenticate.autenticate, ClientControllers.myListFavoritesProduct)
 
 clientRoutes.post('/client/favorite/:idProduct', autenticate.autenticate, ClientControllers.favoriteProduct)
 
-clientRoutes.delete('/client/favorite/:idProduct', ClientControllers.deleteFavoriteProduct)
+clientRoutes.delete('/client/favorite/:idProduct', autenticate.autenticate, ClientControllers.deleteFavoriteProduct)
 
 export default clientRoutes
