561f8e1923d7e8157cb91d191df76e1ba4ae39d7
srwalkerb <srwalkerbdevs@gmail.com>
2021-05-10 18:54:14 -0300

    fix: bug in token

--- src/routes/clientRoutes.ts
+++ src/routes/clientRoutes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import ClientControllers from '../controllers/ClientControllers'
+import autenticate from '../middlewares/autenticate'
 
 const clientRoutes = Router()
 
@@ -7,7 +8,7 @@ clientRoutes.post('/client', ClientControllers.createClient)
 
 clientRoutes.get('/client/favorite', ClientControllers.myListFavoritesProduct)
 
-clientRoutes.post('/client/favorite/:idProduct', ClientControllers.favoriteProduct)
+clientRoutes.post('/client/favorite/:idProduct', autenticate.autenticate, ClientControllers.favoriteProduct)
 
 clientRoutes.delete('/client/favorite/:idProduct', ClientControllers.deleteFavoriteProduct)
 
